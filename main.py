c20d8b337001d28619a90d91b16c118d2e66bc85
Shadowfax1162 <calebhantech@gmail.com>
2020-01-10 21:31:41 -0500

    Added max tries and tried to do 50%, 25%, and 10%- need fixing.

--- main.py
+++ main.py
@@ -13,11 +13,20 @@ while n:
     secret_number = random.randint(0, num)
     print(secret_number)
     i = True
+    max_guess = int(input("Do you want to set a maximum amount of tries? Enter 0 if you don't want a maximum amount of guesses. "))
+    if max_guess == 0:
+        max_guess = 100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+    else:
+        max_guess -= 1
     while i:
         comparison = int(input("Guess an integer from 1 to %d! (If you are continuing, it is the same number. If you won previously, it is a different number.)  " % num))
         #secret_number = random.randint(0, num)
-
-        if comparison == secret_number:
+        if max_guess == count:
+            print("That was incorrect. ")
+            print("Game over. You have reached the maximum amount of tries. ")
+            i = False
+            n = False
+        elif comparison == secret_number:
             count += 1
             print("You got it!")
             print("It took you", count, "tries!")
@@ -32,7 +41,19 @@ while n:
 
         else:
             count += 1
-            if comparison > secret_number:
-                  print("You guessed too high.")
-            else:
-                  print("You guessed too low.")
+            # if comparison > secret_number:
+            #     print("You guessed too high.")
+            if (comparison * 1.5) > secret_number:
+                print("You guessed too high.")
+            elif ((comparison * 1.25) > secret_number) and ((comparison * 1.5) < secret_number):
+                print("You guessed high.")
+            elif ((comparison * 1.10) > secret_number) and ((comparison * 1.25) < secret_number):
+                print("You guessed a little too high.")
+            elif ((comparison * 0.10) < secret_number) and ((comparison * 0.25) > secret_number):
+                print("You guessed a little too low.")
+            elif ((comparison * 0.25) < secret_number) and ((comparison * 0.5) < secret_number):
+                print("You guessed low.")
+            elif (comparison * 0.5) < secret_number:
+                print("You guessed too low.")
+            # else:
+            #     print("You guessed too low.")
